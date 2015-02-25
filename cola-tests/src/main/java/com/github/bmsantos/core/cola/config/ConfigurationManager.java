ff2330b2dc6e31bcd70ae467da7d5c26ed7e1dd1
Bruno Santos <bmrosantos@gmail.com>
2015-02-24 23:57:22 -0500

    Added license header to source
    Issue #4

--- cola-tests/src/main/java/com/github/bmsantos/core/cola/config/ConfigurationManager.java
+++ cola-tests/src/main/java/com/github/bmsantos/core/cola/config/ConfigurationManager.java
@@ -1,4 +1,22 @@
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.bmsantos.core.cola.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,28 +26,20 @@ public enum ConfigurationManager {
 
     config;
 
+    private static Logger log = LoggerFactory.getLogger(ConfigurationManager.class);
+
     private final Properties props = new Properties();
 
     private ConfigurationManager() {
-        loadProperties("application.properties");
-        loadProperties("messages.properties");
+        loadProperties("cola-tests-application.properties");
+        loadProperties("cola-tests-messages.properties");
     }
 
     public void loadProperties(final String name) {
-        InputStream in = null;
-        try {
-            in = ConfigurationManager.class.getResourceAsStream("/" + name);
+        try (final InputStream in = ConfigurationManager.class.getResourceAsStream("/" + name)) {
             props.load(in);
-            in.close();
         } catch (final IOException e) {
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (final Exception e) {
-                    // empty
-                }
-            }
+            log.error("Failed to load configuration properties file: " + name);
         }
     }
 
