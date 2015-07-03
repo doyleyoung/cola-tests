040ee8d4e746fee36367ea054348d53f5a86b31a
Bruno Santos <bmrosantos@gmail.com>
2015-07-02 23:17:12 -0400

    Instrument method removal
    - Added method removal to transformer
    
    Issue #25

--- cola-tests/src/main/java/com/github/bmsantos/core/cola/instrument/ColaTransformer.java
+++ cola-tests/src/main/java/com/github/bmsantos/core/cola/instrument/ColaTransformer.java
@@ -1,5 +1,6 @@
 package com.github.bmsantos.core.cola.instrument;
 
+import static java.util.Arrays.copyOf;
 import static org.objectweb.asm.ClassWriter.COMPUTE_FRAMES;
 import static org.objectweb.asm.ClassWriter.COMPUTE_MAXS;
 
@@ -12,26 +13,45 @@ import org.objectweb.asm.ClassWriter;
 
 import com.github.bmsantos.core.cola.injector.InfoClassVisitor;
 import com.github.bmsantos.core.cola.injector.InjectorClassVisitor;
+import com.github.bmsantos.core.cola.injector.MethodRemoverClassVisitor;
 
 public class ColaTransformer implements ClassFileTransformer {
+
+    private static final int WRITER_FLAGS = COMPUTE_FRAMES | COMPUTE_MAXS;
 
     @Override
     public byte[] transform(final ClassLoader classLoader, final String className, final Class<?> classBeingRedefined,
         final ProtectionDomain protectionDomain, final byte[] classfileBuffer) throws IllegalClassFormatException {
 
+        final byte[] instrumentedBuffer = copyOf(classfileBuffer, classfileBuffer.length);
+
         try {
 
-            final ClassReader reader = new ClassReader(classfileBuffer);
-            final ClassWriter writer = new ClassWriter(reader, COMPUTE_FRAMES | COMPUTE_MAXS);
+            final ClassReader reader = new ClassReader(instrumentedBuffer);
+            final ClassWriter writer = new ClassWriter(reader, WRITER_FLAGS);
             final InfoClassVisitor info = new InfoClassVisitor(writer, classLoader);
             final InjectorClassVisitor injector = new InjectorClassVisitor(info);
             reader.accept(injector, 0);
 
-            return writer.toByteArray();
+            if (!info.getIdeEnabledMethods().isEmpty()) {
+                return removeMethods(writer, info);
+            }
+
+            if (!info.getFeatures().isEmpty() || !info.getIdeEnabledMethods().isEmpty()) {
+                return writer.toByteArray();
+            }
         } catch (final Throwable t) {
             // empty
         }
 
         return classfileBuffer;
+    }
+
+    private byte[] removeMethods(final ClassWriter writer, final InfoClassVisitor info) {
+        final ClassReader rreader = new ClassReader(writer.toByteArray());
+        final ClassWriter rwriter = new ClassWriter(rreader, WRITER_FLAGS);
+        final MethodRemoverClassVisitor remover = new MethodRemoverClassVisitor(rwriter, info.getIdeEnabledMethods());
+        rreader.accept(remover, 0);
+        return rwriter.toByteArray();
     }
 }
--- cola-tests/src/test/java/test/utils/TestUtils.java
+++ cola-tests/src/test/java/test/utils/TestUtils.java
@@ -4,22 +4,28 @@ import static com.github.bmsantos.core.cola.utils.ColaUtils.binaryToOsClass;
 import static org.objectweb.asm.ClassWriter.COMPUTE_FRAMES;
 import static org.objectweb.asm.ClassWriter.COMPUTE_MAXS;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.List;
 
 import org.codehaus.plexus.util.IOUtil;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.util.TraceClassVisitor;
 
 import com.github.bmsantos.core.cola.formatter.FeatureDetails;
 import com.github.bmsantos.core.cola.injector.InfoClassVisitor;
 
 public class TestUtils {
 
+    private static final int WRITER_FLAGS = COMPUTE_FRAMES | COMPUTE_MAXS;
+
     public static List<FeatureDetails> loadFeatures(final String className) throws IOException {
         final ClassReader cr = new ClassReader(className);
-        final ClassWriter cw = new ClassWriter(cr, COMPUTE_FRAMES | COMPUTE_MAXS);
+        final ClassWriter cw = new ClassWriter(cr, WRITER_FLAGS);
         final InfoClassVisitor classVisitor = new InfoClassVisitor(cw, Thread.currentThread().getContextClassLoader());
 
         // When
@@ -33,4 +39,12 @@ public class TestUtils {
         return IOUtil.toByteArray(is);
     }
 
+    public static String traceBytecode(final byte[] bytecode) {
+        final ClassReader cr = new ClassReader(bytecode);
+        final ClassWriter cw = new ClassWriter(cr, WRITER_FLAGS);
+        final ByteArrayOutputStream capture = new ByteArrayOutputStream();
+        final TraceClassVisitor tcv = new TraceClassVisitor(cw, new PrintWriter(new PrintStream(capture), true));
+        cr.accept(tcv, 0);
+        return capture.toString();
+    }
 }
