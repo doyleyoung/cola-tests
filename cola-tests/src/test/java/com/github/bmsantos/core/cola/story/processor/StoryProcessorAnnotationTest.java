1c188d4deac99e5d44b9420995f5e898a08fbef7
Bruno Santos <bmrosantos@gmail.com>
2016-02-11 23:55:24 -0500

    Introduce step alias
    Steps can now be aliased.
    
    Issue #46

--- cola-tests/src/test/java/com/github/bmsantos/core/cola/story/processor/StoryProcessorAnnotationTest.java
+++ cola-tests/src/test/java/com/github/bmsantos/core/cola/story/processor/StoryProcessorAnnotationTest.java
@@ -1,26 +1,26 @@
 package com.github.bmsantos.core.cola.story.processor;
 
-import static com.github.bmsantos.core.cola.report.ReportLoader.reportLoader;
-import static java.util.Arrays.asList;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import gherkin.deps.com.google.gson.Gson;
-
 import java.util.ArrayList;
 import java.util.List;
-
-import org.junit.Before;
-import org.junit.ComparisonFailure;
-import org.junit.Test;
 
 import com.github.bmsantos.core.cola.formatter.ReportDetails;
 import com.github.bmsantos.core.cola.report.Report;
 import com.github.bmsantos.core.cola.story.annotations.Given;
 import com.github.bmsantos.core.cola.story.annotations.Then;
 import com.github.bmsantos.core.cola.story.annotations.When;
+import gherkin.deps.com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.ComparisonFailure;
+import org.junit.Test;
+
+import static com.github.bmsantos.core.cola.report.ReportLoader.reportLoader;
+import static java.lang.Thread.currentThread;
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class StoryProcessorAnnotationTest {
 
@@ -36,6 +36,14 @@ public class StoryProcessorAnnotationTest {
         "Given Beta\n"
             + "When 101 is inserted\n"
             + "Then the true real method will execute";
+
+    private final String aliasStory =
+      "Given a man\n"
+        + "And a woman\n"
+        + "When the man blinks to the woman\n"
+        + "And the woman blinks to the man\n"
+        + "Then the woman will blush\n"
+        + "And the man will blush";
 
     private final String exceptionStory =
         "Given a first method\n"
@@ -137,6 +145,17 @@ public class StoryProcessorAnnotationTest {
     }
 
     @Test
+    public void shouldProcessAliasedSteps() throws Throwable {
+        // When
+        StoryProcessor.process("Feature: I'm a feature", "Scenario: Should Process Story", aliasStory,
+          projectionValues, noReport, instance);
+
+        // Then
+        assertThat(instance.executionOrder, contains("givenAPerson", "givenAPerson", "whenBlinked", "whenBlinked",
+          "thenBabbiesAreBorn", "thenBabbiesAreBorn"));
+    }
+
+    @Test
     public void shouldProcessReports() throws Throwable {
         // Given
         final String reportName = "report";
@@ -174,58 +193,72 @@ public class StoryProcessorAnnotationTest {
         @Given("a first method")
         public void givenFirst() {
             wasGivenCalled = true;
-            executionOrder.add(Thread.currentThread().getStackTrace()[1].getMethodName());
+            executionOrder.add(currentThread().getStackTrace()[1].getMethodName());
         }
 
         @Given("a second method")
         public void givenSecond() {
             wasGivenAndCalled = true;
-            executionOrder.add(Thread.currentThread().getStackTrace()[1].getMethodName());
+            executionOrder.add(currentThread().getStackTrace()[1].getMethodName());
         }
 
         @Given("Alpha|Beta")
         public void givenAlphaOrBeta() {
-            executionOrder.add(Thread.currentThread().getStackTrace()[1].getMethodName());
+            executionOrder.add(currentThread().getStackTrace()[1].getMethodName());
+        }
+
+        @Given({"a man", "a woman"})
+        public void givenAPerson() {
+            executionOrder.add(currentThread().getStackTrace()[1].getMethodName());
         }
 
         @When("the first method is called")
         public void whenFirst() {
             wasWhenCalled = true;
-            executionOrder.add(Thread.currentThread().getStackTrace()[1].getMethodName());
+            executionOrder.add(currentThread().getStackTrace()[1].getMethodName());
         }
 
         @When("the second method is called")
         public void whenSecond() {
             wasWhenAndCalled = true;
-            executionOrder.add(Thread.currentThread().getStackTrace()[1].getMethodName());
+            executionOrder.add(currentThread().getStackTrace()[1].getMethodName());
         }
 
         @When("\\d+ is inserted")
         public void whenANumberIsInserted() {
-            executionOrder.add(Thread.currentThread().getStackTrace()[1].getMethodName());
+            executionOrder.add(currentThread().getStackTrace()[1].getMethodName());
+        }
+
+        @When({"the man blinks to the woman", "the woman blinks to the man"})
+        public void whenBlinked() {
+            executionOrder.add(currentThread().getStackTrace()[1].getMethodName());
         }
 
         @Then("the first method will execute")
         public void thenFirst() {
             wasThenCalled = true;
-            executionOrder.add(Thread.currentThread().getStackTrace()[1].getMethodName());
+            executionOrder.add(currentThread().getStackTrace()[1].getMethodName());
         }
 
         @Then("the second method will execute")
         public void thenSecond() {
             wasThenAndCalled = true;
-            executionOrder.add(Thread.currentThread().getStackTrace()[1].getMethodName());
+            executionOrder.add(currentThread().getStackTrace()[1].getMethodName());
         }
 
         @Then("the (unreal|true real) method will execute")
         public void thenTheRealMethodWillExecute() {
-            executionOrder.add(Thread.currentThread().getStackTrace()[1].getMethodName());
+            executionOrder.add(currentThread().getStackTrace()[1].getMethodName());
         }
 
         @Then("the second method assertion will throw an exception")
         public void thenThrowAnException() throws Exception {
             throw new ComparisonFailure("message", "0", "1");
         }
-    }
 
+        @Then({"the woman will blush", "the man will blush"})
+        public void thenBabbiesAreBorn() {
+            executionOrder.add(currentThread().getStackTrace()[1].getMethodName());
+        }
+    }
 }
