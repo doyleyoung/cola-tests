9444c7da6668b16ade8fa4ab9aa1eaf237993495
Bruno Santos <bmrosantos@gmail.com>
2016-02-20 01:17:24 -0500

    Notify incomplete feature
    The parser does not throw an exception when the feature is incomplete.
    The FeatureValidator validates for expected minimal conditions for a cola test.
    Missing conditions raise an InvalidFeature exception describing the respective cause.
    
    Issue #51

---
+++ cola-tests/src/main/java/com/github/bmsantos/core/cola/formatter/FeatureValidator.java
@@ -0,0 +1,29 @@
+package com.github.bmsantos.core.cola.formatter;
+
+import com.github.bmsantos.core.cola.exceptions.InvalidFeature;
+
+public class FeatureValidator {
+
+    public static FeatureDetails validate(final FeatureDetails feature, final String fromUri) {
+        validateFeature(feature, fromUri);
+        return feature;
+    }
+
+    private static void validateFeature(final FeatureDetails feature, final String fromUri) {
+        final String name = feature.getFeature().getName();
+
+        if (feature.getScenarios().isEmpty()) {
+            throw new InvalidFeature(fromUri + " - Cause: No scenarios found for Feature: " + name);
+        }
+
+        for (final ScenarioDetails scenario: feature.getScenarios()) {
+            validateScenario(name, scenario, fromUri);
+        }
+    }
+
+    private static void validateScenario(final String feature, final ScenarioDetails scenario, final String fromUri) {
+        if (scenario.getSteps().isEmpty()) {
+            throw new InvalidFeature(fromUri + " - Cause: No steps found for Feature: " + feature + " - Scenario: " + scenario.getScenario().getName());
+        }
+    }
+}
